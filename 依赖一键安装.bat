e585153d9e85f17f0e3d4317e177097a46daea98
yuhaodi22222 <148213533+yuhaodi22222@users.noreply.github.com>
2023-10-17 18:57:05 +0800

    Add files via upload

---
+++ 依赖一键安装.bat
@@ -0,0 +1,9 @@
+@echo off
+python -m pip install --upgrade pip -i  https://mirrors.aliyun.com/pypi/simple/
+pip install pyqt5 -i https://mirrors.aliyun.com/pypi/simple/
+pip install PyQt5-tools -i https://mirrors.aliyun.com/pypi/simple/
+pip install qt-material -i https://mirrors.aliyun.com/pypi/simple/
+pip install rich -i https://mirrors.aliyun.com/pypi/simple/
+pip install mss -i https://mirrors.aliyun.com/pypi/simple/
+pip install pygame -i https://mirrors.aliyun.com/pypi/simple/
+pip install pyinstaller -i https://mirrors.aliyun.com/pypi/simple/
