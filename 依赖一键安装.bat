1a1abde70c015dd578e019f0e475748c89842aae
yuhaodi22222 <148213533+yuhaodi22222@users.noreply.github.com>
2024-01-27 15:34:13 +0800

    Delete 依赖一键安装.bat

--- 依赖一键安装.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-python -m pip install --upgrade pip -i  https://mirrors.aliyun.com/pypi/simple/
-pip install pyqt5 -i https://mirrors.aliyun.com/pypi/simple/
-pip install PyQt5-tools -i https://mirrors.aliyun.com/pypi/simple/
-pip install qt-material -i https://mirrors.aliyun.com/pypi/simple/
-pip install rich -i https://mirrors.aliyun.com/pypi/simple/
-pip install pyinstaller -i https://mirrors.aliyun.com/pypi/simple/
